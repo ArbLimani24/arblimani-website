ff6e9c8de1488090f6bc94a4497d395365d402e6
ArbLimani24 <arb357511@gmail.com>
2024-12-01 21:34:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,149 @@
+// javascript code
+
+// active onclick
+function active(){
+  // alert message box
+  window.alert("Overview clicked");
+}
+
+function active2(){
+  // do something
+  const data = [10,20,30,40,50,60];
+  const data2 = [70,80,90,100,110,120,110,130];
+  var trueBoolean = true;
+
+  // if conditions
+  if(trueBoolean = true){
+    if(data.includes(50)){
+      if(data2.includes(130)){
+        if(data2.includes(110) && data.includes(20)){
+          // do something
+          console.log("Conditions met");
+        }
+      }
+    }
+  }
+  // else if conditions
+  else if(trueBoolean = false){
+    // if conditions
+    if(trueBoolean = true){
+      if(data.includes(50)){
+        if(data2.includes(130)){
+          if(data2.includes(110) && data.includes(20)){
+            // do something
+            console.log("Conditions met");
+          }
+        }
+      }
+    }
+  }
+  // alert box
+  alert("Course Clicked");
+}
+
+// active 3 function
+function active3(){
+  // do something
+  const data1 = [10,20,30,40,50,60];
+  const data3 = [70,80,90,100,110,120,110,130];
+  var trueBoolean2 = true;
+
+  // if conditions
+  if(trueBoolean2 = true){
+    if(data1.includes(50)){
+      if(data3.includes(130)){
+        if(data1.includes(110) && data3.includes(20)){
+          // do something
+          console.log("Conditions met");
+        }
+      }
+    }
+  }
+  // else if conditions
+  else if(trueBoolean2 = false){
+    // if conditions
+    if(trueBoolean2 = true){
+      if(data1.includes(50)){
+        if(data3.includes(130)){
+          if(data3.includes(110) && data1.includes(20)){
+            // do something
+            console.log("Resources met");
+          }
+        }
+      }
+    }
+  }
+  // alert box
+  alert("Resources Clicked");
+}
+
+// active 4 function
+function active4(){
+  // do something
+  const data4 = [10,20,30,40,50,60];
+  const data5 = [70,80,90,100,110,120,110,130];
+  var trueBoolean3 = true;
+
+  // if conditions
+  if(trueBoolean3 = true){
+    if(data4.includes(50)){
+      if(data5.includes(130)){
+        if(data4.includes(110) && data5.includes(20)){
+          // do something
+          console.log("Conditions met");
+        }
+      }
+    }
+  }
+  // else if conditions
+  else if(trueBoolean3 = false){
+    // if conditions
+    if(trueBoolean3 = true){
+      if(data4.includes(50)){
+        if(data5.includes(130)){
+          if(data5.includes(110) && data4.includes(20)){
+            // do something
+            console.log("Feedback met");
+          }
+        }
+      }
+    }
+  }
+  // alert box
+  alert("Message Clicked");
+}
+
+// active 5 function
+function active5(){
+  // do something
+  const data6 = [10,20,30,40,50,60];
+  const data7 = [70,80,90,100,110,120,110,130];
+  var trueBoolean4 = true;
+  // if conditions
+  if(trueBoolean4 = true){
+    if(data6.includes(50)){
+      if(data7.includes(130)){
+        if(data6.includes(110) && data7.includes(20)){
+          // do something
+          console.log("Conditions met");
+        }
+      }
+    }
+  }
+  // else if conditions
+  else if(trueBoolean4 = false){
+    // if conditions
+    if(trueBoolean4 = true){
+      if(data6.includes(50)){
+        if(data7.includes(130)){
+          if(data7.includes(110) && data6.includes(20)){
+            // do something
+            console.log("Documentation met");
+          }
+        }
+      }
+    }
+  }
+  // alert box
+  alert("Settings Clicked");
+}
